cfaec3cfa940f0e146ed68a2e32edc4e7ce34dd7
Marga M <82484033+margamaiztegui@users.noreply.github.com>
2025-05-02 18:58:12 +0800

    prep code

---
+++ build/precache-manifest.dcdf314854d0f8796fead442b23bcc86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28c50a0e9ced964dc9895afdd24354a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0e33ad1413371c793c8",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8e1876f07cd5d7779e17",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "b0e33ad1413371c793c8",
+    "url": "./static/js/2.75002904.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.75002904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e1876f07cd5d7779e17",
+    "url": "./static/js/main.5e0e5c4d.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
